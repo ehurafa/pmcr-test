2fe1016ab9b962c1cff1925019544ebc665ca872
Rafael Gomes <rafarfl20102gmail.com>
2019-02-22 17:02:33 -0300

    Create components - Header, Sidebar, Content, Form

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import {MatDialogModule} from '@angular/material/dialog';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
