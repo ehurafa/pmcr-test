144d0e484f44871af003fa6b8d70e53aec0dae6e
Rafael Gomes <rafarfl20102gmail.com>
2019-02-25 21:28:30 -0300

    REMOVE content.component

--- messages-panel/src/app/home/home.component.ts
+++ messages-panel/src/app/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { HeaderComponent } from '../header/header.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
-import { ContentComponent } from '../content/content.component';
+import { ApiService } from '../services/api.service';
+import { Message } from '../Message';
+import { DataSource } from '@angular/cdk/table';
+import { Observable } from 'rxjs/Observable';
+
+
 
 @Component({
   selector: 'home',
@@ -11,9 +15,47 @@ import { ContentComponent } from '../content/content.component';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+ 
+  constructor(private api: ApiService) {
   }
+  
+    
+	// data: Message[] = [];
+	private data: any[];
+
+	isLoadingResults = false;
+  
+   displayedColumns = ['title', 'hour',  'subject', 'body' ,'delete'];
+   //dataSource = new MessageDataSource(this.dataService);
+   
+   
+  
+  ngOnInit() {
+	  /*this.api.getMessages()
+		.subscribe(res => {
+		  this.data = res;
+		  console.log(this.data);
+		  this.isLoadingResults = false;
+		}, err => {
+		  console.log(err);
+		  this.isLoadingResults = false;
+		});      
+		*/
+		
+		this.api.getAll()
+		.subscribe(res => {
+		  this.data = res;
+		  console.log(this.data);
+		  this.isLoadingResults = false;
+		}, err => {
+		  console.log(err);
+		  this.isLoadingResults = false;
+		}); 
+		
+		
+	
+		
+		
+	}
 
 }
